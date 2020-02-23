0a23061eb388a14c066d0d7460fe2738df90ae92
Brandon Liu <bliu1002001@gmail.com>
2020-02-23 02:02:36 -0800

    Parstagram

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,29 @@
+ //
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Brandon Liu on 2/23/20.
+//  Copyright © 2020 Brandon Liu. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
